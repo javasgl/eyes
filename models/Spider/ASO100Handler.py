8ad8ace5dddf26a57fc1c0e060497fba487af72d
ganglin.song <ganglin.song@yourdream.cc>
2017-01-22 14:20:34 +0800

    feat: remove useless code

--- models/Spider/ASO100Handler.py
+++ models/Spider/ASO100Handler.py
@@ -4,14 +4,12 @@
 import urllib2
 import re
 from models.Config.Config import Config
-from models.Logger.Logger import Logger
 from models.Spider.SpiderHandler import SpiderHandler
 from pyquery import PyQuery
 
 
 class ASO100Handler(SpiderHandler):
     URL = 'https://aso100.com/search?country=cn&search=%s'
-    REGEX = '.*<h4 class="media-heading"><a.*>(\d+)、%s.*<\/a><\/h4>.*'
 
     def __init__(self, handler=None):
         super(ASO100Handler, self).__init__(handler)
@@ -29,7 +27,7 @@ class ASO100Handler(SpiderHandler):
             response = urllib2.urlopen(request)
         except urllib2.URLError as e:
             if hasattr(e, 'code'):
-                print 'Error:', e.code, e.reason
+                rank = None
         finally:
             if response:
                 if re.match(ur'.*' + params + '.*', response.geturl()):
@@ -41,12 +39,9 @@ class ASO100Handler(SpiderHandler):
                         group = re.search(ur'(\d+)、' + appname + '', app.text())
                         if group:
                             rank = group.group(1)
-
-                else:
-                    print 'res:%s,%s' % (response.geturl(), type(self))
-                    print 'url:%s,%s' % (url, type(self))
-                    print 'redirected!need alert'
         if rank is not None:
             print rank
+            # send diff mail
+            return 'done!'
         else:
             return None
